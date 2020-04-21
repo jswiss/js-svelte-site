48f609b3d8ddf01584e1ef666839389fa9d2bb79
jswiss <swissjoshua@gmail.com>
2020-04-21 10:51:57 +0100

    set up basic layout with flex

---
+++ src/components/Footer.svelte
@@ -0,0 +1,42 @@
+<style>
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  /* clearfix */
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+
+  li {
+    display: block;
+    float: right;
+  }
+
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    display: block;
+  }
+</style>
+
+<footer>
+  <div id="footer-left" />
+  <ul>
+    <li>
+      <a href=".">GitHub</a>
+    </li>
+    <li>
+      <a href="about">LinkedIn</a>
+    </li>
+    <li>
+      <a href="about">Twitter</a>
+    </li>
+    <li>
+      <a href="about">Email</a>
+    </li>
+  </ul>
+</footer>
