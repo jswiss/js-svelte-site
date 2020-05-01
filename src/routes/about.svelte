69f3db729a60c2c366e4a1c1d164788fecb125cc
jswiss <swissjoshua@gmail.com>
2020-05-01 16:51:36 +0100

    add about mvp

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,17 @@
 <style>
   p {
-    text-align: center;
-    margin: 0 auto;
+    margin: 1em auto;
+    font-size: 1.2em;
+    text-align: left;
   }
   section {
     margin-bottom: 1rem;
   }
 
-  p {
-    margin: 1em auto;
+  .tldr {
+    font-size: 1.3rem;
+    font-weight: 700;
+    font-style: italic;
   }
 </style>
 
@@ -18,12 +21,73 @@
 
 <main>
   <section>
-    <p>Current</p>
+    <p class="tldr">
+      TL;DR - I'm a Pittsburgh-born, former humanitarian worker, current
+      software engineer, based in Glasgow, enjoying being in one place after a
+      long nomadic period.
+    </p>
   </section>
   <section>
-    <p>Past</p>
+    <p>
+      Hey there, I'm Josh! I'm a Glasgow-based Software Engineer, by way of
+      Kenya, South Sudan, Washington DC, The Gambia, and Pittsburgh. I became a
+      programmer in a bit of a roundabout way: I originally studied Public
+      Policy and wanted to be an urban planner, but the 2008 financial crisis
+      forced me into the consulting world, which bored me into a drive for
+      adventure in the aid and development sector across sub-Saharan Africa. My
+      work increasingly required the use of technological tools, and I
+      increasingly wanted to be able to make and adjust the tools myself. I
+      participated in a bootcamp in London in 2015, and have been a software
+      engineer since.
+    </p>
+    <p>
+      Being a Software Engineer satisfies my drive for always learning new
+      things. While staying on top of new technologies, trends, and best
+      practices is a challenge, it's definitely a welcome one.
+    </p>
   </section>
   <section>
-    <p>CV</p>
+    <p>
+      I am primarily a JavaScript developer, with most of my time spent on
+      frontend web development. I do a bit of backend stuff too, and even some
+      DevOps, mostly NodeJS/Express, Postgres, and AWS. For the past year I've
+      written JavaScript almost exclusively with Typescript. Aside from Svelte
+      projects where there is not good Typescript support (yet), I plan on only
+      working in Typescript from now on.
+    </p>
+    <p>
+      Over the years I have had to work with other languages too; PHP, Ruby,
+      Java, etc. I no expert in any of them, but I certainly know enough to
+      navigate an existing code base.
+    </p>
+    <p>
+      I am currently learning and re-learning some languages and frameworks that
+      I write about in my
+      <a href="blog">blog</a>
+      , including:
+    </p>
+    <ul>
+      <li>Rust;</li>
+      <li>CSS-Grid & Flexbox;</li>
+      <li>Svelte; and</li>
+      <li>Docker.</li>
+    </ul>
+  </section>
+  <section>
+    <p>
+      When I'm not writing code I try to get outside and enjoy the Scottish
+      outdoors as much as possible. I love hiking, running, cycling, and
+      foraging. The weather here is not always conducive to outdoor activities,
+      so when I'm stuck inside I like to cook, ferment/pickle what I've foraged,
+      and read, mostly fiction. I might write about this stuff too 😬.
+    </p>
+  </section>
+  <section>
+    <p>
+      If you want to read about whatever; JavaScript, Rust, fermentation,
+      running, life, etc., please check out my
+      <a href="blog">blog</a>
+      .
+    </p>
   </section>
 </main>
